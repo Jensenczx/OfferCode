09122692a74c5df9b591954df0e2f626c4e1dd98
Jensen <chenjx@renyanmail.com>
2016-06-27 19:38:19 +0800

    合并两个链表

--- (18)mergTwoLists/Solution.java
+++ (18)mergTwoLists/Solution.java
@@ -67,6 +67,23 @@ public class Solution {
         return tem;
     }
 
+    public static ListNode Merge(ListNode list1,ListNode list2) {
+        ListNode head = new ListNode(0);
+        while (list1 != null && list2 != null) {
+            head.next = new ListNode(list1.val > list2.val ? list2.val : list1.val);
+            if (head.next.val == list1.val)
+                list1 = list1.next;
+            else list2 = list2.next;
+        }
+        ListNode tmp = (list1 == null ? list1 : list2);
+        while (tmp != null) {
+            head.next = new ListNode(tmp.val);
+            tmp = tmp.next;
+        }
+        return head;
+    }
+
+
     public static void main(String [] agrs){
     	ListNode node1 = new ListNode(1);
 
@@ -94,4 +111,6 @@ public class Solution {
 对于链表问题，经常需要的是对链表进行遍历，所以需要向下走，无论是用来记录的还是被记录的
 都是需要
 注意：空节点的判断，
+
+update:更新后巧妙的借助于三元运算符，来有效的来使得代码变得更加清晰。
 */
