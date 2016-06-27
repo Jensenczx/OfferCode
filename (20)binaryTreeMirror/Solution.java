8a117e56fff424fc4e2bb95db04355e05ae1abcf
Jensen <chenjx@renyanmail.com>
2016-06-27 21:09:02 +0800

    二叉树反转

--- (20)binaryTreeMirror/Solution.java
+++ (20)binaryTreeMirror/Solution.java
@@ -29,6 +29,16 @@ public class Solution {
         }
     }
 
+    public static void Mirror(TreeNode root) {
+        if(root == null)
+            return;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+
     public static void main (String [] args){
     	TreeNode node1 = new TreeNode(1);
     	TreeNode node2 = new TreeNode(2);
